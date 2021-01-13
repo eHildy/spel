f1821d7c1990cc156be939a0a7aaae87246b7dc5
Edward Hildebrandt <ehildebrandt@ara.com>
2021-01-13 17:05:42 -0500

    Comment out unneeded code

--- spel/scripts/vmware.sh
+++ spel/scripts/vmware.sh
@@ -9,15 +9,15 @@ fi
 echo "installing vmware tools"
 yum install open-vm-tools -y
 
-#wget http://192.168.0.185/linux.iso -P /tmp
-mkdir -p /mnt/vmware
-mount -o loop /home/vagrant/linux.iso /mnt/vmware
-
-cd /tmp || exit 1
-tar xzf /mnt/vmware/VMwareTools-*.tar.gz
-
-umount /mnt/vmware
-rm -fr /home/vagrant/linux.iso
-
-/tmp/vmware-tools-distrib/vmware-install.pl -d
-rm -fr /tmp/vmware-tools-distrib
+##wget http://192.168.0.185/linux.iso -P /tmp
+#mkdir -p /mnt/vmware
+#mount -o loop /home/vagrant/linux.iso /mnt/vmware
+#
+#cd /tmp || exit 1
+#tar xzf /mnt/vmware/VMwareTools-*.tar.gz
+#
+#umount /mnt/vmware
+#rm -fr /home/vagrant/linux.iso
+#
+#/tmp/vmware-tools-distrib/vmware-install.pl -d
+#rm -fr /tmp/vmware-tools-distrib
